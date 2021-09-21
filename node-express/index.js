7f722a6fe1a59d20ddbeb0e422b6e25ce051ce36
Ernesto <ebastidasp@unal.edu.co>
2021-09-21 08:43:33 -0500

    Express Generator

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,8 @@ const express = require('express'),
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -16,11 +18,12 @@ app.use(bodyParser.json());
 
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
 
 const server = http.createServer(app);
