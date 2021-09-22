8843b30938d047f286be9f920f22719b3d6fe3a5
Ernesto <ebastidasp@unal.edu.co>
2021-09-22 16:23:52 -0500

    Passport JWT

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,10 +14,11 @@ var usersRouter = require('./routes/users');
 
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -48,21 +49,6 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
