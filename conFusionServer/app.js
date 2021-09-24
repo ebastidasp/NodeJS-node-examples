52faea0325b82d3bb9943d80ec45b93fcab323f6
Ernesto <ebastidasp@unal.edu.co>
2021-09-24 11:21:23 -0500

    File Upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,7 +15,7 @@ var usersRouter = require('./routes/users');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
-
+const uploadRouter = require('./routes/uploadRouter');
 const mongoose = require('mongoose');
 
 const url = config.mongoUrl;
@@ -65,6 +65,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
